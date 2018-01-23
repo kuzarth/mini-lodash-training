60d7417eae52b5dbdd43698812c95144db785542
Arthur Kuz <kuzarth@gmail.com>
2018-01-23 11:19:56 +0300

    Refactored to ES6 Class

--- src/lodash.js
+++ src/lodash.js
@@ -1,43 +1,53 @@
-const _ = (function () {
-  return {
-    chain: function (array) {
-      return Object.assign({}, this, {
-        _array: array,
-        value: function () {
-          const val = this._array
-
-          delete this.value
-          delete this._array
-
-          return val
-        },
-      })
-    },
-    filter: function (arg1, cb) {
-      if (typeof cb !== 'function' && this._array === undefined) {
-        return arg1
-      }
-
-      if (this._array !== undefined) {
-        this._array = _.filter(this._array, arg1)
-        return this
-      }
-
-      return arg1.filter(cb)
-    },
-    map: function (arg1, cb) {
-      if (typeof cb !== 'function' && this._array === undefined) {
-        return arg1
-      }
-
-      if (this._array !== undefined) {
-        this._array = _.map(this._array, arg1)
-        return this
-      }
-
-      return arg1.map(cb)
-    },
+export default class _ {
+  constructor(array) {
+    this._array = array
   }
-})()
 
-export default _
+  static chain(array) {
+    const newLo = new _(array)
+    return newLo
+  }
+
+  static filter(array, cb) {
+    return typeof cb === 'function'
+      ? _.chain(array).filter(cb).value()
+      : array
+  }
+
+  static map(array, cb) {
+    return typeof cb === 'function'
+      ? _.chain(array).map(cb).value()
+      : array
+  }
+
+  value() {
+    return this._array
+  }
+
+  filter(cb) {
+    const { length } = this._array
+    const arr = []
+
+    for (let i = 0; i < length; i++) {
+      if (cb(this._array[i]))
+        arr.push(this._array[i])
+    }
+
+    this._array = arr
+
+    return this
+  }
+
+  map(cb) {
+    const { length } = this._array
+    const arr = []
+
+    for (let i = 0; i < length; i++) {
+      arr.push(cb(this._array[i]))
+    }
+
+    this._array = arr
+
+    return this
+  }
+}
